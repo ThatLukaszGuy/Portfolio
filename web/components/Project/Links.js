326b20c05f5f31777052d0d6ab6c9380538d875b
Lukasz Jakubowski <lukasz.jakubowski@onet.eu>
2022-09-02 17:34:01 +0200

    :egg: handling in case of no deployment of projects

--- web/components/Project/Links.js
+++ web/components/Project/Links.js
@@ -2,8 +2,32 @@ import { Button,Text } from '@chakra-ui/react'
 import React from 'react'
 import { AiFillGithub } from "react-icons/ai"
 import {BiWorld} from 'react-icons/bi'
+import Swal from 'sweetalert2'
+
+
+
 
 export const Links = ({ links }) => {
+  
+  const noLinkAlert = () => {
+    Swal.fire({
+      title: 'No deployment',
+      text: "The deployment of this project does not seem to be a website. Check out the repo if u want to try out the project.",
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#289FED',
+      cancelButtonColor: '#64748b',
+      cancelButtonText: 'Close',
+      confirmButtonText: 'See Repo'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.open(links.github, '_blank')
+      }
+    })
+  }
+  
+  
+  
   return (
     <div className='bg-gray-900 text-white h-[300px] grid grid-cols-1 place-items-center'>
         <div>
@@ -12,7 +36,8 @@ export const Links = ({ links }) => {
             <Button  leftIcon={<AiFillGithub />}  mx={2} my={2} bg={'black'} color='white' _hover={{ bg: 'gray.900' }} onClick={() => window.open(links.github, '_blank')}>
                 View Repo
             </Button>
-            <Button rightIcon={<BiWorld />} mx={2} my={2} bg={'blue.500'} color='white' _hover={{ bg: 'blue.600' }} onClick={() => window.open(links.deployment, '_blank')}>
+            
+            <Button rightIcon={<BiWorld />} mx={2} my={2} bg={'blue.500'} color='white' _hover={{ bg: 'blue.600' }} onClick={ links.deployment == 'nolink' ? () => noLinkAlert() : () => window.open(links.deployment, '_blank') }>
                 View Deployment
             </Button>
         </div>
